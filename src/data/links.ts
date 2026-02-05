44d74662e154d8652dfcba03dc0b9e52bd84d174
Umut Eren Kaplan <kaeruishere@gmail.com>
2026-02-05 22:58:46 +0300

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,9 +1,9 @@
 export const Links = {
-    github: "https://github.com/aathifzahir",
-    linkedin: "https://linkedin.com/in/aathifzahir",
-    email: "mailto:aathifzahir12@gmail.com",
-    resume: "https://www.dropbox.com/scl/fi/q4xrx79y71qi3i01yffja/Aathif_Zahir_CV.pdf?rlkey=7v6aicpcshu6h1diqyq9cb0p2&st=wbf2xkt8&dl=1",
-    emailonly: "aathifzahir12@gmail.com",
-    location: "Colombo, Sri Lanka",
-    phone: "+94 76 527 1411",
+    github: "https://github.com/kaeruishere",
+    linkedin: "https://linkedin.com/in/umuterenkaplan",
+    email: "mailto:kaplanumuteren@gmail.com",
+    resume: "https://errenn.com/cv",
+    emailonly: "kaplanumuteren@gmail.com",
+    location: "İzmir, Türkiye",
+    phone: "+90 532 159 4577",
   };
